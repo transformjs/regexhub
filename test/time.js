7f13caaabecc911706d08dde0bf2f207e9c62afc
zhongchiyu <zhongchiyu@gmail.com>
2014-09-19 18:41:08 +0800

    feat: add time regex

---
+++ test/time.js
@@ -0,0 +1,29 @@
+var hub = require('../');
+
+describe('time', function() {
+    var regex = new RegExp('^' + hub.time + '$');
+
+    var valid = [
+        '12:01:45',
+        '01:45',
+        '1:45'
+    ];
+
+    var invalid = [
+        '12-01-45',
+        '33:01:45',
+        '12:45:1'
+    ];
+
+    it('valid', function() {
+        valid.forEach(function(item) {
+            regex.test(item).should.be.true;
+        })
+    });
+
+    it('invalid', function() {
+        invalid.forEach(function(item) {
+            regex.test(item).should.be.false;
+        })
+    });
+});
