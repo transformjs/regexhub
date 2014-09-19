3e4d41618124a3d8f092c269071bfc5bc6fb9425
Chiyu Zhong <1606069+CatTail@users.noreply.github.com>
2014-09-19 17:39:37 +0800

    feat: add domain name regex

---
+++ test/domain.js
@@ -0,0 +1,32 @@
+var hub = require('../');
+
+describe('domain', function() {
+    var regex = new RegExp('^' + hub.domain + '$');
+
+    var valid = [
+        'example.com',
+        'example2.com',
+        'example-3.com',
+        'example---3.com',
+        'example33-3.com',
+        'example.com2',
+        'example.com-2',
+    ];
+
+    var invalid = [
+        '5example.com',
+        'example-.com',
+    ];
+
+    it('valid', function() {
+        valid.forEach(function(item) {
+            regex.test(item).should.be.true;
+        });
+    });
+
+    it('invalid', function() {
+        invalid.forEach(function(item) {
+            regex.test(item).should.be.false;
+        });
+    });
+});
